68e81d873b10bbcb46f4a86fc3d12cd948bb6c2d
Tasuku Saito <nkc.tasuku.saito@gmail.com>
2017-10-26 10:42:57 +0900

    プレハブ作成

---
+++ Assets/bulletController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletController : MonoBehaviour {
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    // Use this for initialization
+    void Start () {
+        //shoot(new Vector3(0, 200, 2000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/bulletGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletGenerator : MonoBehaviour {
+    public GameObject bulletPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            bullet.GetComponent<bulletController>().shoot(worldDir.normalized * 2000);
+        }
+	}
+}
